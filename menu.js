f9524cefa2c20881ca22886d509e636db3638274
joinVazquez <emiliolouback@gmail.com>
2022-10-06 17:35:54 -0300

    V1

---
+++ menu.js
@@ -0,0 +1,14 @@
+document.write (
+
+"<li id='nav1' class='nav-item' style='padding-right: 1.5rem;'><a class='nav-link' href='/convite.html'>Como fazer parte?</a> </li>",
+"<li id='nav2' class='nav-item'><a class='nav-link active' href='index.html'>Portal</a></li>",
+"<li id='nav3' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Categorias </a><div class='dropdown-menu'><a class='dropdown-item' href='packs.html'>Packs</a>",
+"<a class='dropdown-item' href='mockups.html'>Mockups</a>",
+
+
+
+
+"<li id='nav8' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Perfil</a><div class='dropdown-menu'>",
+"<a class='dropdown-item' onclick='logout()'>Sair</a>",
+"</div>"
+);
