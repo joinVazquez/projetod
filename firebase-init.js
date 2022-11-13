dd8c51c8a74669fc214629fa5920a2fd6961d7e8
joinVazquez <emiliolouback@gmail.com>
2022-11-12 22:05:21 -0300

    new login

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZCSlKyJxzQGWQvUoX5qpcP78_8pf8Zaw",
+    authDomain: "nextpack-io.firebaseapp.com",
+    databaseURL: "https://nextpack-io.firebaseio.com",
+    projectId: "nextpack-io",
+    storageBucket: "nextpack-io.appspot.com",
+    messagingSenderId: "688401667087",
+    appId: "1:688401667087:web:8b644046756f029a287f7a",
+    measurementId: "G-FGZ79EBPXL"
+    };
+   firebase.initializeApp(firebaseConfig);
